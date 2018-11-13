472216c48638db71625bb2acdfde703210f90463
VERBACK-TWO\VerBack-Aop <nitirat@vertice-international.com>
2018-11-13 11:52:00 +0700

    update layout

--- src/conponents/VideoItem.js
+++ src/conponents/VideoItem.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 
-class VideoItem extends Component{
+class VideoItem extends Component {
 
-    render(){
-        return(
-            <div className="col-2">
-                <a href={"https://www.youtube.com/watch?v="+this.props.video.id.videoId} target="_blank"><img src={this.props.video.snippet.thumbnails.default.url} /></a>
+    render() {
+        return (
+            <div>
+                <div className="col-2">
+                    <a href={"https://www.youtube.com/watch?v=" + this.props.video.id.videoId} target="_blank"><img src={this.props.video.snippet.thumbnails.default.url} /></a>
+                </div>
                 <hr />
             </div>
+
         )
     }
 }
